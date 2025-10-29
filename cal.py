98f49bafc17dad8193fc6803ebc1947ac3073442
sumeetby16 <sumeetby16>
2025-10-29 22:13:49 +0530

    mul

--- cal.py
+++ cal.py
@@ -3,5 +3,12 @@ b=float(intput("enter second number:"))
 print("Addition:",a+b)
 print("Subtraction:",a-b)
 
+a=float(intput("enter first number:"))
+b=float(intput("enter second number:"))
+print("multiplication:",a*b)
+print("Division:",a/b)
+
+
+
 
 
